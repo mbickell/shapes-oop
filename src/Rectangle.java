e8646d3cd18972f619578ca232203398e8b3f1b4
Matthew Bickell <matthewbickell@hotmail.com>
2026-03-23 10:32:12 +0200

    feat: finish implementation of rectangle

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,3 +1,36 @@
-public class Rectangle {
+public class Rectangle extends Shape {
+  double width;
+  double height;
 
+  Rectangle(double width, double height, String color, boolean transparent) {
+    super(color, transparent);
+    this.width = width;
+    this.height = height;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public void setWidth(double width) {
+    this.width = width;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  public void setHeight(double height) {
+    this.height = height;
+  }
+
+  @Override
+  double area() {
+    return width * height;
+  }
+
+  @Override
+  String draw() {
+    return "Drawing Rectangle";
+  }
 }
