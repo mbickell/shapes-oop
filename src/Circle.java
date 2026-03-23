7128ecbd7dbb25c1676290a6301b23f7c34a1c09
Matthew Bickell <matthewbickell@hotmail.com>
2026-03-23 10:35:08 +0200

    feat: finish implementation of circle

--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,26 @@
-public class Circle {
+public class Circle extends Shape {
+  double radius;
 
+  Circle(double radius, String color, boolean transparent) {
+    super(color, transparent);
+    this.radius = radius;
+  }
+
+  public double getRadius() {
+    return radius;
+  }
+
+  public void setRadius(double radius) {
+    this.radius = radius;
+  }
+
+  @Override
+  double area() {
+    return Math.PI * (Math.pow(radius, 2));
+  }
+
+  @Override
+  String draw() {
+    return "Drawing Circle";
+  }
 }
